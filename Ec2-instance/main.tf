416f2f02aff87f14af889a39ade423ee2ce6d069
Thanhlam43k4 <lamcoivodoi123@gmail.com>
2023-11-03 14:18:54 +0700

    Update file terraform config aws instance for jenkins server

--- Ec2-instance/main.tf
+++ Ec2-instance/main.tf
@@ -3,19 +3,25 @@
 provider "aws" {
     region = "ap-northeast-1"
 }
-locals{
-    ami_id = "ami-0bcf3ca5a6483feba"
-    key_id = "to-do-key"
-    instance = "t2.micro"
-}
+
 #Create an EC2 instance for the master node
-resource "aws_instance" "jenkins_instance" {
-  ami = local.ami_id
-  instance_type = local.instance
-  key_name = local.key_id
-  vpc_security_group_ids = ["sg-0c10680ff7c539c1e"]
-  user_data = file("user-data-jenkins.sh")
-  tags = {
-    Name = "Jenkins"
-  }
+module "vpc"{
+  source = "./vpc-module"
+  vpc_cidr_block = "16.0.0.0/16"
+  public_subnet = "16.0.1.0/24"
+  zone-public-subnet = "ap-northeast-1a"
+}
+
+module "ec2"{
+  source = "./ec2-module"
+  ami_id = "ami-0d48337b7d3c86f62"
+  vpc_id = module.vpc.vpc_id
+  key_id = "to-do-key"
+  zone = "ap-northeast-1a"
+  instance_type = "t2.micro"
+  user-data = "user-data-jenkins.sh"
+
+}
+output "ec2-public-ip"{
+  value = module.ec2.ec2-public-ip
 }
